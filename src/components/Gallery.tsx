52600827d67b7adf1008a69e506ba087b0c0caf2
nishantXnova <paudelnishant15@gmail.com>
2025-10-14 21:32:55 +0545

    Updated Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,7 @@
 export default function Gallery() {
   const images = [
     {
-      url: 'https://images.pexels.com/photos/7045715/pexels-photo-7045715.jpeg?auto=compress&cs=tinysrgb&w=800',
+      url: 'https://scontent.fktm7-1.fna.fbcdn.net/v/t1.15752-9/546700167_769476035831420_8655265697408261672_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=109&ccb=1-7&_nc_sid=0024fc&_nc_ohc=_Ox8qwUT17UQ7kNvwHI2H3m&_nc_oc=Adls4gVDi03XuJR0NuhFujuyX_BJvDu-20_TeSI5EFYpvQJ86TV-dbYvTXmCwJcWVXdLxdG6j4hVxkhssaySyAWv&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.fktm7-1.fna&oh=03_Q7cD3gFU-mywIOoeLSjSXA0568q0ClB_eqxXHkYeO9E8YdF6UA&oe=6915EB7C',
       title: 'Taekwondo Training'
     },
     {
@@ -9,7 +9,7 @@ export default function Gallery() {
       title: 'Dance Sessions'
     },
     {
-      url: 'https://images.pexels.com/photos/4662438/pexels-photo-4662438.jpeg?auto=compress&cs=tinysrgb&w=800',
+      url: 'https://www.bing.com/images/search?view=detailV2&ccid=U5FQNwjB&id=79F337B11FDC768DFDDD2FEA213F896EA731BE4C&thid=OIP.U5FQNwjBkkF4Qt6MeyVWLAHaE6&mediaurl=https%3a%2f%2fi.pinimg.com%2foriginals%2faa%2f59%2fe2%2faa59e25b035f095faa77e1ee7b3646cc.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.5391503708c192417842de8c7b25562c%3frik%3dTL4xp26JPyHqLw%26pid%3dImgRaw%26r%3d0&exph=1590&expw=2400&q=zumba+classes+4k+image&FORM=IRPRST&ck=266EA53EF78BD0254693215384C9FABB&selectedIndex=1&itb=0',
       title: 'Zumba Classes'
     },
     {
@@ -21,7 +21,7 @@ export default function Gallery() {
       title: 'Fitness Workouts'
     },
     {
-      url: 'https://images.pexels.com/photos/3775593/pexels-photo-3775593.jpeg?auto=compress&cs=tinysrgb&w=800',
+      url: 'https://media.istockphoto.com/photos/performance-picture-id93958169?k=20&m=93958169&s=612x612&w=0&h=32jpZ-M69wK0YOcxGDCk8HG_JwK__Sod96IP7NB62GM=',
       title: 'Acting Classes'
     }
   ];
